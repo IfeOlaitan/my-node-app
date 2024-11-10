0b25df3d570834a96d0df88f053674a049f4ba04
ifeolaitan <ifeoluwa1201@gmail.com>
2024-11-10 18:55:44 +0000

    update deployment

--- src/components/home.js
+++ src/components/home.js
@@ -134,7 +134,7 @@ scrollContainer.scrollLeft =
           <p id="hmre2">
             {" "}
             <span id="hmre46">Connectin</span>g Talents worldwide: your next Gig
-            awaits you today and make money
+            awaits you.
           </p>
         </div>
 
